278fcd6864abb0ec8e182528893b20c7229a3fb9
Randyhall91 <randyrocks11@gmail.com>
2022-12-13 11:53:20 -0700

    RealTime Data Hydration complete

--- pages/[username]/[slug].jsx
+++ pages/[username]/[slug].jsx
@@ -1,9 +1,59 @@
 
 import React from 'react'
-export default function PostPage({ }) {
+import { firestore, getUserWithUsername, postToJSON } from '../../lib/firebase.js'
+import { useDocumentData } from 'react-firebase-hooks/firestore'
+import styles from '../../styles/Post.module.css';
+import PostContent from '../../components/PostContent.jsx'
+export async function getStaticProps({ params }) {
+  const { username, slug } = params
+  const userDoc = await getUserWithUsername(username)
+
+  let post
+  let path
+
+  if (userDoc) {
+    const postRef = userDoc.ref.collection('posts').doc(slug)
+    post = postToJSON(await postRef.get())
+    path = postRef.path
+  }
+  return {
+    props: { post, path },
+    revalidate: 500,
+  }
+}
+
+export async function getStaticPaths() {
+  const snapshot = await firestore.collectionGroup('posts').get()
+
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data()
+    return {
+      params: { username, slug },
+    }
+  })
+  return {
+    paths,
+    fallback: 'blocking',
+  }
+}
+
+
+export default function Post(props) {
+  const postRef = firestore.doc(props.path)
+  const [realtimePost] = useDocumentData(postRef)
+
+  const post = realtimePost || props.post
+
   return (
-    <main>
-      <h1>Page</h1>
+    <main className={styles.container}>
+      <section>
+        <PostContent post={post} />
+      </section>
+      <aside className='card'>
+        <p>
+          <strong>{post.heartCount || 0}💟</strong>
+        </p>
+      </aside>
     </main>
   )
 }
